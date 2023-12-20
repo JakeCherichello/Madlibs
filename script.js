e04dcc35cd5f04126319c96598020321e83e6aeb
JakeCherichello <Cherichello1104@icloud.com>
2023-12-20 10:33:07 -0500

    did footer

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
-let color = prompt('Enter a color:')
+let adjective = prompt('Enter an adjective:')
+let place = prompt('Enter a location:')
+let food = prompt('Enter a food:')
+let drink = prompt('Enter a drink:')
 let name = prompt('Enter a name:')
- let warm = prompt('Enter a warm country:')
- 
+let emotion = prompt('Enter a emotion:')
 
- let story=`Once upon a time, a ${color} Snowman named ${name} went for a trip to ${warm}`
+let story = `On a ${adjective} winter day, I decided to build a snowman! I built it in the middle of ${place} until my snow man was finally done. 
+I began to get tired so I made ${food} and drank ${drink}. I named my snowman ${name} he was ${number} feet tall and I loved him! I went to sleep 
+thinking of my snowman. When I woke up he was Melted! I felt ${emotion} so that day I stayed at home and ${verb}`
 
- let madLibOutputDiv = document.getElementById('madLibOutput')
+let madLibOutputDiv = document.getElementById('madLibOutput')
 
- madLibOutputDiv.innerHTML = `<p> ${story} </p>`
+madLibOutputDiv.innerHTML = `<p> ${story} </p>`
